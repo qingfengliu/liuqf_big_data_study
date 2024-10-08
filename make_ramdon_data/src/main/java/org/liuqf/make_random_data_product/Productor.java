7fc37c275b6c3f4edcccf2707f7b502adc629a1c
liuqingfeng <504772813@qq.com>
2024-10-08 22:49:15 +0800

    将测试代码转移到生产者

--- make_ramdon_data/src/main/java/org/liuqf/make_random_data_product/Productor.java
+++ make_ramdon_data/src/main/java/org/liuqf/make_random_data_product/Productor.java
@@ -5,20 +5,53 @@ import java.util.Properties;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.liuqf.test_faker_data01.Test;
+
 public class Productor {
-    public static void main(String[] args) {
-        Properties prop = new Properties();
+    private cun_random_data random_data;
+    KafkaProducer<String, String> producer;
+    Properties prop;
+
+    Productor() {
+        this.prop = new Properties();
         prop.put("bootstrap.servers", "hadoop1:9092,hadoop2:9092,hadoop3:9092");
         prop.put("key.serializer", StringSerializer.class.getName());
         prop.put("value.serializer", StringSerializer.class.getName());
-        String topic = "test";
-        KafkaProducer<String, String> producer = new KafkaProducer(prop);
+        this.producer = new KafkaProducer(prop);
 
-        for(int i = 0; i < 10; ++i) {
-            producer.send(new ProducerRecord(topic, "hello kafka" + i));
+        this.random_data = new cun_random_data();
+    }
+
+    Productor(long timebreak,int run_times,long sleep_time) {
+        this();
+        this.random_data.setRun_times(run_times);
+        this.random_data.setTimebreak(timebreak);
+        this.random_data.setSleep_time(sleep_time);
+    }
+
+    public static void main(String[] args) {
+        Productor test = new Productor(1000 * 60 * 60, 10, 1000 * 60);
+        test.run();
+    }
+
+    public void run() {
+        while (true) {
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < random_data.getRun_times(); i++) {
+                String data = random_data.get_data();
+                producer.send(new org.apache.kafka.clients.producer.ProducerRecord<>("test", data));
+                System.out.println(data);
+            }
+            long end = System.currentTimeMillis();
+            if (end - start < random_data.getTimebreak()) {
+                try {
+                    Thread.sleep(random_data.getSleep_time());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                break;
+            }
         }
-
-        producer.close();
-        System.out.println("Hello world!");
     }
 }
--- make_ramdon_data/src/main/java/org/liuqf/test_faker_data01/Test.java
+++ make_ramdon_data/src/main/java/org/liuqf/test_faker_data01/Test.java
@@ -14,50 +14,5 @@ import java.util.Properties;
 import static net.datafaker.transformations.Field.field;
 
 public class Test {
-    private cun_random_data random_data;
-    KafkaProducer<String, String> producer;
-    Properties prop;
 
-    Test() {
-        this.prop = new Properties();
-        prop.put("bootstrap.servers", "hadoop1:9092,hadoop2:9092,hadoop3:9092");
-        prop.put("key.serializer", StringSerializer.class.getName());
-        prop.put("value.serializer", StringSerializer.class.getName());
-        this.producer = new KafkaProducer(prop);
-
-        this.random_data = new cun_random_data();
-    }
-
-    Test(long timebreak,int run_times,long sleep_time) {
-        this();
-        this.random_data.setRun_times(run_times);
-        this.random_data.setTimebreak(timebreak);
-        this.random_data.setSleep_time(sleep_time);
-    }
-
-    public static void main(String[] args) {
-        Test test = new Test(1000 * 60 * 60, 10, 1000 * 60);
-        test.run();
-    }
-
-    public void run() {
-        while (true) {
-            long start = System.currentTimeMillis();
-            for (int i = 0; i < random_data.getRun_times(); i++) {
-                String data = random_data.get_data();
-                producer.send(new org.apache.kafka.clients.producer.ProducerRecord<>("test", data));
-                System.out.println(data);
-            }
-            long end = System.currentTimeMillis();
-            if (end - start < random_data.getTimebreak()) {
-                try {
-                    Thread.sleep(random_data.getSleep_time());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                break;
-            }
-        }
-    }
 }
