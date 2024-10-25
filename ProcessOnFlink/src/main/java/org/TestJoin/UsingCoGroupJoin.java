75db9428580c812a61dbde5bfaa30ce802157145
liuqingfeng <504772813@qq.com>
2024-10-26 04:38:34 +0800

    cogroupprocess实验完毕

--- ProcessOnFlink/src/main/java/org/TestJoin/UsingCoGroupJoin.java
+++ ProcessOnFlink/src/main/java/org/TestJoin/UsingCoGroupJoin.java
@@ -14,16 +14,16 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.CoProcessFunction;
-import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
 import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 import org.json.JSONObject;
 
 import java.util.Iterator;
 
 public class UsingCoGroupJoin {
     public static Tuple2<DataStream, DataStream> setup(StreamExecutionEnvironment env){
-
         //sale data source
+
         KafkaSource<String> KfkSaleDataSource = KafkaSource.<String>builder()
                 .setBootstrapServers("hadoop1:9092,hadoop2:9092,hadoop3:9092")
                 .setTopics("sale_random_data")
@@ -75,52 +75,45 @@ public class UsingCoGroupJoin {
         return new Tuple2<DataStream,DataStream>(sale_data,person_data);
     }
 
-    static CoProcessFunction<Tuple2<String, String>, Tuple2<String, String>, String> myCoProcessFunction = new CoProcessFunction<Tuple2<String, String>, Tuple2<String, String>, String>() {
+    public static class myCoProcessFunction extends CoProcessFunction<Tuple2<String, String>, Tuple2<String, String>, String>{
+
+        private static final long WAIT_TIME = 5000L;
 
         // 某个key在processElement1中存入的状态
         private ValueState<String> state1;
 
         // 某个key在processElement2中存入的状态
         private ValueState<String> state2;
+
+        // 如果创建了定时器，就在状态中保存定时器的key
+        private ValueState<Long> timerState;
+
+        // onTimer中拿不到当前key，只能提前保存在状态中（KeyedProcessFunction的OnTimerContext有API可以取到，但是CoProcessFunction的OnTimerContext却没有）
+        private ValueState<String> currentKeyState;
+
+        //两个侧输出流
+        private final OutputTag<String> source1SideOutput = new OutputTag<String>("left-output"){};
+        private final OutputTag<String> source2SideOutput = new OutputTag<String>("right-output"){};
+
+
+
         @Override
         public void open(Configuration parameters) throws Exception {
             // 初始化状态
             state1 = getRuntimeContext().getState(new ValueStateDescriptor<>("myState1", String.class));
             state2 = getRuntimeContext().getState(new ValueStateDescriptor<>("myState2", String.class));
+            timerState = getRuntimeContext().getState(new ValueStateDescriptor<>("timerState", Long.class));
+            currentKeyState = getRuntimeContext().getState(new ValueStateDescriptor<>("currentKeyState", String.class));
         }
 
-        @Override
-        public void processElement1(Tuple2<String, String> value, Context ctx, Collector<String> out) throws Exception {
-
-//            String key = value.f0;
-            String value2 = state2.value();
-            System.out.println("value1处理函数");
-            if (value2 != null) {
-                //2号流收到过[{}]，值是[{}];
-                out.collect(chuli(value.f1, value2));
-                // 把2号流的状态清理掉
-                state2.clear();
-            }else{
-                //2号流还未收到过[{}]，把1号流收到的值[{}]保存起来
-
-                state1.update(value.f1);
-            }
-        }
-
-        @Override
-        public void processElement2(Tuple2<String, String> value, Context ctx, Collector<String> out) throws Exception {
-            //            String key = value.f0;
-            String value1 = state1.value();
-            System.out.println("value2处理函数");
-            if (value1 != null) {
-                //1号流收到过[{}]，值是[{}];
-                out.collect(chuli(value.f1, value1));
-                // 把2号流的状态清理掉
-                state1.clear();
-            }else{
-                //2号流还未收到过[{}]，把1号流收到的值[{}]保存起来
-                state2.update(value.f1);
-            }
+        /**
+         * 所有状态都清理掉
+         */
+        private void clearAllState() {
+            state1.clear();
+            state2.clear();
+            currentKeyState.clear();
+            timerState.clear();
         }
 
         public String chuli(String first, String second) throws Exception {
@@ -144,6 +137,94 @@ public class UsingCoGroupJoin {
             }
             return jsonObject1.toString();
         }
+
+        @Override
+        public void processElement1(Tuple2<String, String> value, Context ctx, Collector<String> out) throws Exception {
+
+            String key = value.f0;
+            String value2 = state2.value();
+            if (value2 != null) {
+                //2号流收到过数据那么执行join流程,并且删除定时器。删除定时器的时候需要那个传入的超时时间
+                System.out.println("process left in and process right not null");
+                System.out.println("process left in and process right not null currentWatermark:"+ctx.timerService().currentWatermark());
+                out.collect(chuli(value.f1, value2));
+                long timerKey = timerState.value();
+                ctx.timerService().deleteEventTimeTimer(timerKey);
+                //所有状态清空
+                clearAllState();
+            }else{
+                System.out.println("process left in and process right null");
+                //2号流没有收到数据,那么建立定时器。建立定时器传入超时时间。
+                state1.update(value.f1);
+
+                currentKeyState.update(key);
+                long timerKey = ctx.timestamp()+WAIT_TIME;
+
+                System.out.println("process left in and process right null currentWatermark:"+ctx.timerService().currentWatermark());
+                System.out.println("process left in and process right null timerKey:"+timerKey);
+//                System.out.println("registerEventTimeTimer");
+                ctx.timerService().registerEventTimeTimer(timerKey);
+                //保存定时器的key
+                timerState.update(timerKey);
+
+
+            }
+        }
+
+        @Override
+        public void processElement2(Tuple2<String, String> value, Context ctx, Collector<String> out) throws Exception {
+            String key = value.f0;
+            String value1 = state1.value();
+            if (value1 != null) {
+                System.out.println("process right in and process left not null");
+                System.out.println("process left in and process left not null currentWatermark:"+ctx.timerService().currentWatermark());
+                //1号流收到过数据，触发join，并且删除定时器
+                out.collect(chuli(value.f1, value1));
+
+                long timerKey = timerState.value();
+                ctx.timerService().deleteEventTimeTimer(timerKey);
+
+                clearAllState();
+            }else{
+                System.out.println("process right in and process left null");
+                //1号流如果没收到数据,注册定时器，注册定时器的时候需要一个超时时间
+                state2.update(value.f1);
+                currentKeyState.update(key);
+
+                // 开始10秒的定时器，10秒后会进入
+                long timerKey = ctx.timestamp() + WAIT_TIME;
+                System.out.println("process right in and process left null currentWatermark:"+ctx.timerService().currentWatermark());
+                System.out.println("process right in and process left null timerKey:"+timerKey);
+//                System.out.println("registerEventTimeTimer");
+                ctx.timerService().registerEventTimeTimer(timerKey);
+                // 保存定时器的key
+                timerState.update(timerKey);
+            }
+        }
+
+        @Override
+        public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
+//            super.onTimer(timestamp, ctx, out);
+            // 定时器触发，说明另一个流没有数据，所以清空所有状态
+
+            String value1 = state1.value();
+            String value2 = state2.value();
+            System.out.println("process on time on key:" + currentKeyState.value());
+//            System.out.println("process on time currentWatermark:"+ctx.timerService().currentWatermark());
+//            System.out.println("process on time timestamp:"+ctx.timestamp());//这个是打印了超时时间
+            if(null!=value1) {
+                // 侧输出
+                ctx.output(source1SideOutput, value1);
+            }
+
+            if(null!=value2) {
+                // 侧输出
+                ctx.output(source2SideOutput, value2);
+            }
+
+            clearAllState();
+        }
+
     };
 
     public static void main(String[] args) {
@@ -163,7 +244,7 @@ public class UsingCoGroupJoin {
             public String getKey(Tuple2<String, String> value) throws Exception {
                 return value.f0;
             }
-        })).process(myCoProcessFunction).map(
+        })).process(new myCoProcessFunction()).map(
                 new MapFunction<String, String>() {
                     @Override
                     public String map(String value) {
