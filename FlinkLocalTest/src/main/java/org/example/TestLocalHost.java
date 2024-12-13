244ed556d02c1f98146630b1bb834c5a150d072e
庆丰 <504772813@qq.com>
2024-12-13 13:19:28 +0800

    本地运营flink

--- FlinkLocalTest/src/main/java/org/example/TestLocalHost.java
+++ FlinkLocalTest/src/main/java/org/example/TestLocalHost.java
@@ -1,4 +1,7 @@
 package org.example;
+import LowLevelFunction.MyKeyedProcessFunction;
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -9,33 +12,41 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TestLocalHost {
 
-    //dataframe builder
-    static DataStreamSource setup_from_socker(StreamExecutionEnvironment env, int port) {
-        DataStreamSource<String> lines = env.socketTextStream("localhost", 9999);
-        return lines;
-    }
+
 
     public static void main(String[] args) {
+        Configuration config = new Configuration();
+        // 设置配置参数（尽管 heartbeat.timeout 在本地环境中可能不起作用）
+        config.setString("heartbeat.timeout", "60000"); // 设置心跳超时为60秒
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(config);
 
-        Configuration conf = new Configuration();
-        conf.setInteger("rest.port", 8081);
-        conf.setString("rest.address", "localhost");
-
-        //create a StreamExecutionEnvironment
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
         //create webui,需要依赖flink-runtime-web。但是有时候即使maven配置了也不出现在idea的lib中。需要手动加一下
         //idea->project structure->modules->lib->+->from maven用maven repository的buildr可以搜索到
         //启动一个时候web仅有job,如果停止了job，webui也会停止
+        //Configuration conf = new Configuration();
+//        conf.setInteger("rest.port", 8081);
+//        conf.setString("rest.address", "localhost");
 //        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
 
         //socket data source
         //需要先启动nc -lk 9999。否则报错
-        DataStreamSource sales = setup_from_socker(env, 9999);
-        DataStreamSource person = setup_from_socker(env, 9998);
+        DataStreamSource<String> sales_source = env.socketTextStream("localhost", 9999);
+        //创建一个带时间戳的流
+        SingleOutputStreamOperator<String> sales = sales_source.<String>assignTimestampsAndWatermarks(
+                WatermarkStrategy.<String>forMonotonousTimestamps()//有序流.如果是无序流，可以使用forBoundedOutOfOrderness
+                        .withTimestampAssigner((s, timestamp) -> {
+                            String[] parts = s.split(",");
+                            System.out.println(parts[parts.length - 1]);
+                            return Long.parseLong(parts[parts.length - 1]);
+                        })
+        );
 
 
-        DataStreamSource<String> lines = env.socketTextStream("localhost", 9999);
-        lines.print();
+        sales.keyBy(s -> s.split(",")[0])
+                .process(new MyKeyedProcessFunction())
+                .print();
+
+
         try {
             env.execute("Test");
         } catch (Exception e) {
