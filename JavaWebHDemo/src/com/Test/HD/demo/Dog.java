7dfef5aea3f23a085f6206f468d21fe32404580f
liuqingfeng <504772813@qq.com>
2025-01-07 22:56:06 +0800

    20250107因为git ignore不知道怎么被改了导致版本回退

---
+++ JavaWebHDemo/src/com/Test/HD/demo/Dog.java
@@ -0,0 +1,37 @@
+package com.Test.HD.demo;
+
+import java.util.Objects;
+
+public class Dog {
+    //getter and setter methods
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(name, dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    Dog() {
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ Study_JavaWeb/html_test/js_test/js/button.js
@@ -0,0 +1,3 @@
+function suprise2(){
+  alert("我是外链式引入的js");
+}
