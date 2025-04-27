ecd7ab2145c193b4b1b8943cb0f037125223ca46
liuqingfeng <504772813@qq.com>
2025-04-27 22:59:14 +0800

    测试flink sql window join

--- studyflink/GoToRestaurant/src/main/java/org/liuqf/MainWindowTag.java
+++ studyflink/GoToRestaurant/src/main/java/org/liuqf/MainWindowTag.java
@@ -1,13 +1,12 @@
 package org.liuqf;
 
-
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.types.Row;
 
-public class Main {
+public class MainWindowTag {
     public static void main(String[] args) throws Exception {
 
         //1. env-准备环境
@@ -34,18 +33,25 @@ public class Main {
                 "\t'properties.bootstrap.servers'='hadoop1:9092,hadoop2:9092,hadoop3:9092',\n" +
                 "\t'properties.group.id'='abc',\n" +
                 "\t'scan.startup.mode' = 'latest-offset',\n" +
-                "\t'format'= 'json'\n" +
+                "\t'format'= 'json' \n" +
                 ");");
-
-        Table resultTable = tEnv.sqlQuery("SELECT food,price,sum(gmv) over(PARTITION BY food \n" +
-                "ORDER BY ts\n" +
-                "RANGE BETWEEN INTERVAL '30' MINUTE PRECEDING AND CURRENT ROW\n" +
-                ") as money\n" +
-                "FROM \n" +
-                "sale_order\n" +
-                ";");
+//        tEnv.executeSql("SELECT name,sum(gmv) gmv FROM TABLE(\n" +
+//                "\tTUMBLE(TABLE sale_order, DESCRIPTOR(ts), INTERVAL '1' MINUTES)\n" +
+//                ")\n" +
+//                "group by name\n" +
+//                ";").print();   //这个打印出现在运行程序的窗口
+        Table resultTable = tEnv.sqlQuery("SELECT window_start,window_end,sum(gmv) gmv FROM TABLE(\n" +
+                "\tTUMBLE(TABLE sale_order, DESCRIPTOR(ts), INTERVAL '1' MINUTES)\n" +
+                ")\n" +
+                "group by window_start,window_end;");
         DataStream<Row> resultStream = tEnv.toChangelogStream(resultTable);
+//        Table resultTable = tEnv.sqlQuery("select * from sale_order");
+//
+//        // interpret the insert-only Table as a DataStream again
+//        DataStream<Row> resultStream = tEnv.toDataStream(resultTable);
 
+
+        // add a printing sink and execute in DataStream API
         resultStream.print();//这个在webui上可以看到
         env.execute();
 
